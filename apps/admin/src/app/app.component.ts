96e8fc935b053f1ff4be19482eb298ff96126a2b
Oleh Kazban <oleh.kazban@globallogic.com>
2022-09-20 20:35:57 +0300

    NX MFE example

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfe-demo-root',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent {}
---
+++ apps/admin/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfe-demo-admin-entry',
+  template: `<mfe-demo-nx-welcome></mfe-demo-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
